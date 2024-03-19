2c13b9804c8500f3c0e771ee13c1a61c21b3c4b9
Marta Piscitelli <158267636+piscimarta@users.noreply.github.com>
2024-03-19 10:34:52 +0100

    added header with usefuls

---
+++ header.hpp
@@ -0,0 +1,20 @@
+
+// include guard
+#ifndef __header_hpp__
+#define __header_hpp__
+
+// include headers
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+
+// Return a string with a double in scientific notation
+std::string scientific_format(double d, const int &width, const int &prec);
+
+// Return a string with a vector<double> in scientific notation
+std::string scientific_format(const std::vector<double> &v, const int &width, const int &prec);
+
+
+#endif // end of include guard 
